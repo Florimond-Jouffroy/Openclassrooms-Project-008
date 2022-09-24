bb8cdec6541d4a884d6b86c76dad38b8adf49313
Florimond Jouffroy <florimond.jouffroy@gmail.com>
2022-09-24 14:43:29 -0400

    Legacy user create

---
+++ src/Controller/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserController extends AbstractController
+{
+  public function __construct(
+    private UserPasswordHasherInterface $passwordHasher,
+    private EntityManagerInterface $em,
+  ) {
+  }
+
+  #[Route('/users/create', name: "user_create")]
+  public function register(Request $request)
+  {
+    $form = $this->createForm(RegistrationType::class, $user = new User());
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $user->setPassword($this->passwordHasher->hashPassword($user, $user->getPassword()));
+      $this->em->persist($user);
+      $this->em->flush();
+
+      $this->addFlash('success', "L'utilisateur a bien été ajouté.");
+      return $this->redirectToRoute('homepage');
+    }
+
+    return $this->render('user/create.html.twig', [
+      'form' => $form->createView()
+    ]);
+  }
+}
